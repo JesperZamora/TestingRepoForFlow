17a04776b0b15ea453199e77aaa6d095ac9706d0
JesperZamora <jesp78m6@stud.kea.dk>
2024-02-27 13:00:58 +0100

    second commit from feature branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //This was made from feature branch: testbranch1
+        System.out.println("Hi from testbranch1");
     }
 }
